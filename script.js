4df737dadc22984bb28f9273438a3899b53827c9
Sebastian Prestele <51739799+brashd@users.noreply.github.com>
2021-06-14 21:43:16 +0200

    Upload project

---
+++ script.js
@@ -0,0 +1,64 @@
+
+let cards = []
+let sum = 0
+
+let hasBlackjack = false
+let isAlive = false
+
+let message = ""
+
+function getRandomNumber() { 
+    let randomNumber = Math.floor(Math.random()*13)+1
+    if  (randomNumber === 1) { 
+        return 11
+    } else if (randomNumber >= 10){
+        return 10
+    } else
+    return randomNumber
+} 
+
+function startGame () {
+    isAlive = true
+    let firstCard = getRandomNumber()
+    let secondCard = getRandomNumber()
+    sum = firstCard + secondCard 
+    cards = [firstCard,secondCard]
+    renderGame()
+}
+
+
+function renderGame () {
+   
+    document.getElementById("sum-el").innerText = "Sum: " + sum
+    for (let i = 0; i < cards.length;  i++) {
+    document.querySelector("#cards-el").innerText = "Cards: " + cards + " "
+    // console.log(cards[i])
+    }
+
+    if (sum <= 20) {
+        message = "Do you want to draw a new card? 🙂"
+             // console.log("Do you want to draw a new card? 🙂")
+    } else if (sum === 21) {
+        message = "Wohoo! You've got Blackjack! 🥳"
+        // console.log("Wohoo! You've got Blackjack! 🥳")
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game! 😭"
+        // console.log("You're out of the game! 😭")
+        isAlive = false
+    }
+    document.getElementById("message-el").innerText = message
+}
+
+function draw () {
+
+if (isAlive === true && hasBlackjack === false) {
+    let newCard = getRandomNumber()    
+    sum += newCard
+    cards.push(newCard)
+    renderGame()
+    }
+}
+
+
+
